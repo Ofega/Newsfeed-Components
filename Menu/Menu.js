b0bbc2717ddd0257488e8f5bd079f5601e52d899
ofega <fegaeze@gmail.com>
2019-09-04 14:35:10 +0100

    added extra functionality to menu component

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,43 +1,59 @@
-/* This is the data we will be using, study it but don't change anything, yet. */
+(function() {
+  // STATE DECLARATION
+  const state = {
+    isOpen: false,
+    menuItems: [
+      'Students',
+      'Faculty',
+      "What's New",
+      'Tech Trends',
+      'Music',
+      'Log Out'
+    ]
+  }
 
-let menuItems = [
-  'Students',
-  'Faculty',
-  "What's New",
-  'Tech Trends',
-  'Music',
-  'Log Out'
-];
+  // VARIABLE ASSIGNMENT TO DOM ELEMENT
+  const btn = document.querySelector('.menu-button');
 
-// VARIABLE ASSIGNMENT TO DOM ELEMENT
-const btn = document.querySelector('.menu-button');
+  // MENU MAKER FUNCTION
+  function menuMaker(menuItems) {
 
+    // CREATE NEW NODES
+    const menu = document.createElement('div');
+    const menuList = document.createElement('ul');
 
-// MENU MAKER FUNCTION
-function menuMaker(menuItems) {
+    // CREATE NEW MENU ITEMS AND ATTACH TO MENULIST
+    menuItems.map(item => {
+      const menuItem = document.createElement('li');
+      menuItem.textContent = item;
+      menuList.appendChild(menuItem);
+    })
 
-  // CREATE NEW NODES
-  const menu = document.createElement('div');
-  const menuList = document.createElement('ul');
+    // ADD NECESSARY CLASSES
+    menu.classList.add('menu');
 
-  // CREATE NEW MENU ITEMS AND ATTACH TO MENULIST
-  menuItems.map(item => {
-    const menuItem = document.createElement('li');
-    menuItem.textContent = item;
-    menuList.appendChild(menuItem);
-  })
+    // EVENT LISTENER
+    btn.addEventListener('click', () => {
+      event.stopPropagation();
+      if(!state.isOpen) {
+        state.isOpen = true;
+        menu.classList.toggle('menu--open');
+      } else {
+        state.isOpen = false;
+        menu.classList.toggle('menu--open');
+      }
+    });
 
-  // ADD NECESSARY CLASSES
-  menu.classList.add('menu');
+    document.body.addEventListener('click', () => {
+      if(state.isOpen) {
+        state.isOpen = false;
+        menu.classList.toggle('menu--open');
+      }
+    });
 
-  // EVENT LISTENER
-  btn.addEventListener('click', () => {
-    menu.classList.toggle('menu--open');
-  });
+    menu.appendChild(menuList);
+    return menu;
+  }
 
-
-  menu.appendChild(menuList);
-  return menu;
-}
-
-document.body.prepend(menuMaker(menuItems));
+  document.body.prepend(menuMaker(state.menuItems));
+})();
